6bbc0c1fe520e47da14025782321f04ee9b306b5
Monysathya <monysathya.suos@sompom.com>
2019-12-18 15:40:26 +0700

    fix path import NavigationExtras to compatible with angular 8

--- src/RouteModal.ts
+++ src/RouteModal.ts
@@ -1,5 +1,5 @@
 import {Component, Input, Output, EventEmitter, ElementRef, ViewChild, OnDestroy, OnInit} from "@angular/core";
-import {NavigationExtras} from "@angular/router/src/router";
+import {NavigationExtras} from "@angular/router";
 import {Router, ActivatedRoute} from "@angular/router";
 
 @Component({
